64aec3e05d8d24f016545e47ed7262dc51c47184
kristoforusbryant <kristoforusbryant@gmail.com>
2021-01-31 15:04:29 +0800

    minor bug fixes

--- run_exp.py
+++ run_exp.py
@@ -29,9 +29,6 @@ Prior = importlib.import_module("dists.priors." + CONFIG['PRIOR']).Prior
 Proposal = importlib.import_module("dists.proposals." + CONFIG['PROPOSAL']).Proposal
 Likelihood = importlib.import_module("dists.likelihoods." + CONFIG['LIKELIHOOD']).Likelihood
 
-assert Prior.ParamType() == Proposal.ParamType(), "Prior and Proposal have different Parameter Types"
-assert Prior.ParamType() == Likelihood.ParamType(), "Prior and Proposal have different Parameter Types"
-
 ### Load Params and Data 
 filename = os.path.join(exppath, 'Params.pkl')
 with open(filename, 'rb') as handle:
@@ -48,11 +45,11 @@ for i in range(len(DATA)):
     reps = CONFIG["N_REPS"][i]
     
     # Initialise prior, prop, lik, data 
-    prior = Prior(n)
-    prop = Proposal(n)
+    prior = Prior(n, PARAMS[i].__class__, basis=PARAMS[i]._basis)
+    prop = Proposal(n, PARAMS[i].__class__)
     delta = 3 
     D = np.eye(n) # (delta, D) hyperpriors
-    lik = Likelihood(DATA[i], 3, D)
+    lik = Likelihood(DATA[i], 3, D, PARAMS[i].__class__)
     
     sampler = MCMC_Sampler(prior, prop, lik, DATA[i], reps=reps)
     sampler.Run(CONFIG['ITER'][i], summarize=True)
@@ -90,7 +87,7 @@ for i in range(len(DATA)):
     g.Draw(ax = axes[1], pos=pos) 
     axes[1].set_title('predicted', fontsize=20)
     
-    # Plot decreasing likelihoods
+    # Plot decreasing likelihoods (change ncols in the axes to 3 if we want to do this)
     data = DATA[i]
     U = data.transpose() @ data 
     res = {'IG_prior': [],'IG_postr': []}

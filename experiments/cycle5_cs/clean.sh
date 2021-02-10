4ac7a837e7c7122d3be45f9c96ac7a3c47983c0b
kristoforusbryant <kristoforusbryant@gmail.com>
2021-02-10 13:45:21 +0800

    minor updates to clean.sh functions

--- experiments/cycle5_cs/clean.sh
+++ experiments/cycle5_cs/clean.sh
@@ -4,4 +4,7 @@ DIR=$(dirname $0)
 [ -e "$DIR/Params.pkl" ] && rm "$DIR/Params.pkl"
 [ -e "$DIR/K_matrices.pkl" ] && rm "$DIR/K_matrices.pkl"
 [ -e "$DIR/log.dat" ] && rm "$DIR/log.dat"
+rm "$DIR/res"/*
+rm "$DIR/summary"/*
+rm "$DIR/vis"/* 
 

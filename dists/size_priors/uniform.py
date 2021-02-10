5a03c67e6852c52a96960b8f2db9898d5a95d16d
kristoforusbryant <kristoforusbryant@gmail.com>
2021-02-10 13:24:10 +0800

    add uniform count and size priors

---
+++ dists/size_priors/uniform.py
@@ -0,0 +1,10 @@
+# Uniform for Basis count
+class CountPrior: 
+    # Assume Finite Support
+    def __init__(self, params=None):
+        pass 
+    
+    def __call__(self, size):
+        return 1
+    
+    

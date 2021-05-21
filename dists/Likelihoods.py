9f05d5e3b9df23f7983b4be73f2a1aea0d2bf01b
kristoforusbryant <kristoforusbryant@gmail.com>
2021-05-21 23:10:35 +0800

    Added IG_LA

--- dists/Likelihoods.py
+++ dists/Likelihoods.py
@@ -27,3 +27,28 @@ class GW:
 
     def ParamType(self):
         return self._Param.__name__
+
+class GW_LA:
+    def __init__(self, data, delta, D, Param):
+        param = Param(D.shape[0])
+        # GW: G-Wishart(G, delta, D)
+        self.GW_prior = G_Wishart(param, delta, D)
+        self._D = D
+        self._U = data.transpose() @ data
+        D_star = D + self._U
+        self.GW_posterior = G_Wishart(param, delta + data.shape[0], D_star)
+        self._Param = Param
+
+    def Move(self, param_):
+        self.GW_prior.G.SetFromG(param_)
+        self.GW_posterior.G.SetFromG(param_)
+        D_star = self._D + self._U
+        self.GW_posterior.SetD(D_star)
+
+    def PDF(self, param_=None):
+        if param_ is not None:
+            self.Move(param_)
+        return self.GW_posterior.IG_LA() - self.GW_prior.IG_LA()# as log prob
+
+    def ParamType(self):
+        return self._Param.__name__

ed678a00de9cfeb9472bb97d0b069a1f6260f16c
kristoforusbryant <kristoforusbryant@gmail.com>
2021-05-21 22:50:36 +0800

    fixed thresholding bug

--- tests/test_generate_data.py
+++ tests/test_generate_data.py
@@ -7,12 +7,13 @@ from tests.test_utils.generate_data import generate_data
 import numpy as np
 
 def test_generate_data():
+    np.random.seed(123)
     for _ in range(10):
         n = 10
         m = 10000
         g = Graph(n)
         g.SetRandom()
-        X = generate_data(n, m, g)
+        X = generate_data(n, m, g, threshold=1e-6)
 
         a = g.GetAdjM()[np.triu_indices(n, 1)]
         b = (np.abs(np.linalg.inv((X.transpose() @ X) / (m-1))[np.triu_indices(n, 1)]) > 0.1).astype(int)

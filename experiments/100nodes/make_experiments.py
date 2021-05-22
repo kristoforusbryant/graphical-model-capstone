8dcad9f087de951ae8e14f0d7be90edf6df16de6
kristoforusbryant <kristoforusbryant@gmail.com>
2021-05-22 17:24:16 +0800

    Added experiments

---
+++ experiments/100nodes/make_experiments.py
@@ -0,0 +1,44 @@
+from networkx.classes import graph
+from dists.Params import GraphAndBasis
+from dists.CountPriors import TruncatedNB
+from dists.Priors import BasisCount
+from dists.TreePriors import Uniform
+from utils.generate_basis import circle_graph
+from tests.test_utils.generate_data import generate_data_and_save
+
+import numpy as np
+import networkx as nx
+
+# Number of nodes and observations
+n, n_obs = 100, 1000
+
+# Empty Graph
+empty = GraphAndBasis(n)
+
+# Circle Graph
+circle = GraphAndBasis(n, circle_graph(n))
+
+# Random Graphs from the Cycle space
+print("Generating Graphs from the Cycle space...")
+ct_prior = TruncatedNB(n, .75)
+prior = BasisCount(n, GraphAndBasis, ct_prior, Uniform(n))
+np.random.seed(123)
+random0 = prior.Sample()
+random1 = prior.Sample()
+
+# Random Graphs from the Cycle space complement
+print("Generating Graphs from the Cycle space complement...")
+np.random.seed(123)
+random2 = GraphAndBasis(n, nx.to_dict_of_lists(nx.erdos_renyi_graph(n, .2)))
+random3 = GraphAndBasis(n, nx.to_dict_of_lists(nx.barabasi_albert_graph(n, 2)))
+
+# List of graphs
+graphs = [empty, circle, random0, random1, random2, random3]
+graph_names = ['empty', 'circle', 'random0', 'random1', 'random2', 'random3']
+
+# Generate data
+for i in range(len(graphs)):
+    g = graphs[i]
+    outfile = "data/" + graph_names[i] + '.dat'
+    print('Generating ' + outfile + '...' )
+    generate_data_and_save(n, n_obs, g, outfile, threshold=None, seed=123)
---
+++ experiments/100nodes/runner.py
@@ -0,0 +1,69 @@
+from multiprocessing import Pool
+from utils.cbmcmc import cbmcmc
+
+edgeIO = [ ('data/empty.dat', 'results/edge_empty_samples.pkl'),
+           ('data/circle.dat', 'results/edge_circle_samples.pkl'),
+           ('data/random0.dat', 'results/edge_random0_samples.pkl'),
+           ('data/random1.dat', 'results/edge_random1_samples.pkl'),
+           ('data/random2.dat', 'results/edge_random2_samples.pkl'),
+           ('data/random3.dat', 'results/edge_random3_samples.pkl'), ]
+
+allIO = [ ('data/empty.dat', 'results/all_empty_samples.pkl'),
+           ('data/circle.dat', 'results/all_circle_samples.pkl'),
+           ('data/random0.dat', 'results/all_random0_samples.pkl'),
+           ('data/random1.dat', 'results/all_random1_samples.pkl'),
+           ('data/random2.dat', 'results/all_random2_samples.pkl'),
+           ('data/random3.dat', 'results/all_random3_samples.pkl'), ]
+
+hubIO = [ ('data/empty.dat', 'results/hub_empty_samples.pkl'),
+           ('data/circle.dat', 'results/hub_circle_samples.pkl'),
+           ('data/random0.dat', 'results/hub_random0_samples.pkl'),
+           ('data/random1.dat', 'results/hub_random1_samples.pkl'),
+           ('data/random2.dat', 'results/hub_random2_samples.pkl'),
+           ('data/random3.dat', 'results/hub_random3_samples.pkl'), ]
+
+pathIO = [ ('data/empty.dat', 'results/path_empty_samples.pkl'),
+           ('data/circle.dat', 'results/path_circle_samples.pkl'),
+           ('data/random0.dat', 'results/path_random0_samples.pkl'),
+           ('data/random1.dat', 'results/path_random1_samples.pkl'),
+           ('data/random2.dat', 'results/path_random2_samples.pkl'),
+           ('data/random3.dat', 'results/path_random3_samples.pkl'), ]
+
+
+it = 1000
+r, p = (6, .75)
+cob_freq = 100
+seed = 123
+
+# Edge
+def edge_runner(t):
+    d, o = t
+    cbmcmc(d, o, it, 'edge')
+    return 0
+p = Pool()
+p.map(edge_runner, edgeIO)
+
+# # All
+# def all_runner(t):
+#     d, o = t
+#     cbmcmc(d, o, it, 'cycle', 'all', r, p, cob_freq)
+#     return 0
+# p = Pool()
+# p.map(lambda d, o: cbmcmc(d, o, it, 'cycle', 'all', r, p, cob_freq), allIO)
+
+# # Hub
+# def all_runner(t):
+#     d, o = t
+#     cbmcmc(d, o, it, 'cycle', 'hub', r, p, cob_freq)
+#     return 0
+# p = Pool()
+# p.map(lambda d, o: cbmcmc(d, o, it, 'cycle', 'hub', r, p, cob_freq), hubIO)
+
+# # Path
+# def all_runner(t):
+#     d, o = t
+#     cbmcmc(d, o, it, 'cycle', 'path', r, p, cob_freq)
+#     return 0
+# p = Pool()
+# p.map(lambda d, o: cbmcmc(d, o, it, 'cycle', 'path', r, p, cob_freq), pathIO)
+

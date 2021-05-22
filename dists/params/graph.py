6b8bd13e4abafaaec5075d2df14c5c4900deff36
kristoforusbryant <kristoforusbryant@gmail.com>
2021-05-22 10:26:13 +0800

    removed junk from previous release

--- dists/params/graph.py
+++
@@ -1,8 +0,0 @@
-from utils.Graph import Graph
-
-# (Graph, Basis, Active Basis) 
-class Param(Graph):
-    def __init__(self, n, dol=None, basis=None):
-        super().__init__(n, dol)
-        self._basis = basis # list of DOLs
-        

e6fedcfff42acaf819d1999037f954cc3f2a68a4
kristoforusbryant <kristoforusbryant@gmail.com>
2021-05-24 00:08:54 +0800

    cleaned up experiments

--- experiments/cycle_five/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR=$(dirname $0) 
-[ -e "$DIR/Data.pkl" ] && rm "$DIR/Data.pkl"
-[ -e "$DIR/Params.pkl" ] && rm "$DIR/Params.pkl"
-[ -e "$DIR/K_matrices.pkl" ] && rm "$DIR/K_matrices.pkl"
-
-

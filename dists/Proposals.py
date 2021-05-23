fb5786dcf713e5feb2667f68bc06e61b287e1387
kristoforusbryant <kristoforusbryant@gmail.com>
2021-05-23 16:35:03 +0800

    updated proposal to the new change basis routine

--- dists/Proposals.py
+++ dists/Proposals.py
@@ -1,7 +1,5 @@
 import numpy as np
-from utils.generate_basis import cycle_basis, COBM
-from utils.inverse_binary import inverse_GF2, GF2
-
+from utils.generate_basis import change_basis
 
 class BasisWalk:
     def __init__(self, n, Param, tree_prior=None, skip=None):
@@ -16,36 +14,31 @@ class BasisWalk:
     __name__ = 'basis_size_with_tree_prior'
 
     def Sample(self, param):
-        # When basis changes
+        # When basis can change
         if self._skip is not None:
-            if self.counter % self._skip != 0:
-                param_ = param
-            else:
-                M = COBM(param._tree)
+            if self.counter % self._skip == 0:
+                # too much of a hassle to revert by adding bases, just add the entire object
                 self._temp = param.copy()
+
                 param_ = param
 
-                # Make one switch first
+                # move once
                 i = np.random.choice(range(param_._basis.shape[1]))
                 param_.BinAddOneBasis(i)
 
-                # Sample tree that generates a new basis (assume T is uniform, hence does not affect proposal)
+                # change basis
                 T_ = self._tree_prior.Sample()
+                param_ = change_basis(param_, T_)
 
-                param_._basis = cycle_basis(T_)
-                param_._tree = T_
+                # move again
+                i = np.random.choice(range(param_._basis.shape[1]))
+                param_.BinAddOneBasis(i)
+            else:
+                param_ = param
 
-                M_ = COBM(param_._tree) # M and M_ are COBM in the entire space
-                subM = (inverse_GF2(M_) @ GF2(M) % 2)[:param_._basis.shape[1], :param_._basis.shape[1]]
-                _basis_active = (subM @ param_._basis_active % 2)
-                param_._basis_active = _basis_active.astype(bool)
-
-            self.counter += 1
-
-            # Same procedure as basis_size.py
-            i = np.random.choice(range(param_._basis.shape[1]))
-            param_.BinAddOneBasis(i)
-            self._temp = i
+                i = np.random.choice(range(param_._basis.shape[1]))
+                param_.BinAddOneBasis(i)
+                self._temp = i
 
         # When basis stays constant
         else:
@@ -55,18 +48,19 @@ class BasisWalk:
             param_.BinAddOneBasis(i)
             self._temp = i
 
+        self.counter += 1
+
         return param_
 
     def Revert(self,p_):
         if self._skip is None:
             p_.BinAddOneBasis(self._temp)
             return p_
-        elif self.counter % self._skip != 0:
+        elif (self.counter - 1) % self._skip != 0:
             p_.BinAddOneBasis(self._temp)
             return p_
         else:
             return self._temp
-
 
     def PDF(self, p, p_):
         return 0
--- tests/test_proposal.py
+++ tests/test_proposal.py
@@ -21,17 +21,44 @@ def one_edge_difference(g, g_):
 
 def test_proposal_Sample_no_COB():
     n = 10
-    ct_prior = TruncatedNB(6, .75)
+    ct_prior = TruncatedNB(n, .75)
     prior = BasisCount(n, GraphAndBasis, ct_prior, Uniform(n))
     prop = BasisWalk(n, GraphAndBasis, Uniform(n))
     np.random.seed(123)
-    for _ in range(20):
+    for _ in range(50):
         g = prior.Sample()
         g_ = prop.Sample(g.copy())
 
+        print(prop._temp)
+        assert(isinstance(prop._temp, np.integer))
         assert(g._tree == g_._tree)
         assert(one_edge_difference(g, g_))
         assert(in_cycle_space(g_))
         assert(g_is_sum_of_basis(g_))
 
 
+def test_proposal_Sample_with_COB():
+    n = 10
+    cob_freq = 2
+    ct_prior = TruncatedNB(n, .75)
+    prior = BasisCount(n, GraphAndBasis, ct_prior, Uniform(n))
+    prop = BasisWalk(n, GraphAndBasis, Uniform(n), skip = cob_freq)
+    np.random.seed(123)
+
+    g = prior.Sample()
+    for _ in range(100):
+        if prop.counter % cob_freq == 0:
+            g_ = prop.Sample(g.copy())
+            assert(isinstance(prop._temp, GraphAndBasis))
+            assert(in_cycle_space(g_))
+            assert(g_is_sum_of_basis(g_))
+            g = g_
+
+        else:
+            g_ = prop.Sample(g.copy())
+            assert(isinstance(prop._temp, np.integer))
+            assert(g._tree == g_._tree)
+            assert(one_edge_difference(g, g_))
+            assert(in_cycle_space(g_))
+            assert(g_is_sum_of_basis(g_))
+            g = g_

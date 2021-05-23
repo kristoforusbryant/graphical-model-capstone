218b149da061b95098033e7dec0c62d517e24072
kristoforusbryant <kristoforusbryant@gmail.com>
2021-05-23 14:41:56 +0800

    basis is now GF2 np.array type

--- utils/generate_basis.py
+++ utils/generate_basis.py
@@ -1,6 +1,8 @@
 from itertools import product
 from utils.Graph import Graph
 import numpy as np
+import galois
+GF2 = galois.GF(2)
 
 def complete_path(n):
     dol = {i:[i-1,i+1] for i in range(1,n-1)}
@@ -31,36 +33,53 @@ def circle_graph(n):
     return dol
 
 def edge_basis(n):
-    triu = np.triu_indices(n,1)
-    basis = []
-    for i, j in list(zip(triu[0], triu[1])):
-        g = Graph(n)
-        g.AddEdge(i,j)
-        basis.append(g.copy())
-    return basis
+    m = n * (n - 1) // 2
+    return GF2(np.eye(m).astype(int))
+
 
 def cycle_basis(T):
-    # TODO: check if T is a spanning tree of its nodes
-    basis = []
+    n = len(T)
+    m = n * (n - 1) // 2
+    cb = (n - 1) * (n - 2) // 2
+    B = GF2(np.zeros((m, cb)).astype(int))
+    idx = 0
     for i in range(len(T)):
         for j in range(i):
             if j in T._dol[i]: continue
             T_ = T.copy()
             T_.AddEdge(i,j)
-            basis.append(T_.GetSubgraph(T_.find_one_cycle(i, -1,[])))
-    return basis
+            B[:, idx] = T_.GetSubgraph(T_.find_one_cycle(i, -1,[])).GetBinaryL()
+            idx += 1
+    return B
+
+def cycle_basis_complete(T):
+    m = len(T) * (len(T) - 1) // 2
+    B = GF2(np.zeros((m, m)).astype(int))
+    idx = 0
+    for i in range(len(T)):
+        for j in range(i):
+            if j in T._dol[i]: continue
+            T_ = T.copy()
+            T_.AddEdge(i,j)
+            B[:, idx] = T_.GetSubgraph(T_.find_one_cycle(i, -1,[])).GetBinaryL()
+            idx += 1
+    for i in np.where(T.GetBinaryL())[0]:
+        B[i, idx] = 1
+        idx += 1
+    return B
 
 def cut_basis(T):
     # Assuming Complete Graph
-    cutbasis = []
+    B = np.zeros((len(T) - 1, len(T) - 1).astype(int))
+    idx = 0
     for i,j in T.GetEdgeL():
         T_minus_e = T.copy()
         T_minus_e.RemoveEdge(i,j)
         g = Graph(len(T_minus_e))
         g.SetFromEdgeL(list(product(T_minus_e.ConnectedTo(i), T_minus_e.ConnectedTo(j))))
-        g.RemoveEdge(i,j)
-        cutbasis.append(g.copy())
-    return cutbasis
+        B[:, idx] = g.GetBinaryL()
+        idx += 1
+    return B
 
 
 # change of basis matrix M: current -> std
@@ -70,3 +89,4 @@ def COBM(T):
     for i in range(len(basis)):
         M[:,i] = basis[i].GetBinaryL()
     return M
+

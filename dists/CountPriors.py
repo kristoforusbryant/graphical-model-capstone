24b71e512cb2864a3d13fd7548a49e5a44930154
kristoforusbryant <kristoforusbryant@gmail.com>
2021-07-10 14:09:41 +0800

    Use gammaln trick to compute TruncatedNB PMF

--- dists/CountPriors.py
+++ dists/CountPriors.py
@@ -1,14 +1,17 @@
-from scipy.stats import nbinom
+import numpy as np
+from scipy import special
+
+def chooseln(N, k):
+    return special.gammaln(N+1) - special.gammaln(N-k+1) - special.gammaln(k+1)
 
 class TruncatedNB:
     # Assume Finite Support
     def __init__(self, r, p):
         self._r = r
         self._p = p
-        self._rv = nbinom(self._r, self._p)
 
     def __call__(self, size):
-        return self._rv.pmf(size)
+        return chooseln(size + self._r - 1, size) + self._r * np.log(1-self._p) + size * np.log(self._p)
 
 class Uniform:
     # Assume Finite Support

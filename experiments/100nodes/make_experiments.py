5273dff5429fe166e2a81630e2df45d57a359d2a
kris <kristoforusbryant@gmail.com>
2021-07-27 10:53:45 +0000

    added more tests

--- experiments/100nodes/make_experiments.py
+++ experiments/100nodes/make_experiments.py
@@ -1,7 +1,7 @@
 from networkx.classes import graph
 from dists.Params import GraphAndBasis
 from dists.CountPriors import TruncatedNB
-from dists.Priors import BasisCount
+from dists.Priors import BasisCount, BasisInclusion
 from dists.TreePriors import Uniform
 from utils.generate_basis import circle_graph
 from tests.test_utils.generate_data import generate_data_and_save
@@ -21,7 +21,7 @@ circle = GraphAndBasis(n, circle_graph(n))
 
 # Random Graphs from the Cycle space
 print("Generating Graphs from the Cycle space...")
-ct_prior = TruncatedNB(n, .75)
+ct_prior = TruncatedNB(1, .5)
 prior = BasisCount(n, GraphAndBasis, ct_prior, Uniform(n))
 np.random.seed(123)
 random0 = prior.Sample()
@@ -29,25 +29,22 @@ random1 = prior.Sample()
 
 # Random Graphs from the Cycle space complement
 print("Generating Graphs from the Cycle space complement...")
-random2 = GraphAndBasis(n, nx.to_dict_of_lists(nx.erdos_renyi_graph(n, .2, seed=123)))
+random2 = GraphAndBasis(n, nx.to_dict_of_lists(nx.erdos_renyi_graph(n, .05, seed=123)))
 random3 = GraphAndBasis(n, nx.to_dict_of_lists(nx.barabasi_albert_graph(n, 2, seed=123)))
 
+prior = BasisInclusion(n, GraphAndBasis, alpha=.05, tree_prior=Uniform(n))
+np.random.seed(123)
+random4 = prior.Sample()
+random5 = prior.Sample()
+
 # List of graphs
-graphs = [empty, circle, random0, random1, random2, random3]
-graph_names = ['empty', 'circle', 'random0', 'random1', 'random2', 'random3']
+graphs = [empty, circle, random0, random1, random2, random3, random4, random5]
+names = ['empty', 'circle', 'random0', 'random1', 'random2', 'random3', 'random4', 'random5']
 
-# Generate data
-for i in range(len(graphs)):
-    g = graphs[i]
-    outfile = "data/" + graph_names[i] + '.dat'
-    print('Generating ' + outfile + '...' )
-    generate_data_and_save(n, n_obs, g, outfile, threshold=None, seed=123)
+for i in range(len(names)):
+    generate_data_and_save(n, n_obs, graphs[i], f"data/{names[i]}_{n}_{n_obs}.dat", threshold=None, seed=123)
 
-# Saving graphs
-for i in range(len(graphs)):
-    g = graphs[i]
-    g.SetName(graph_names[i])
-    outfile = "data/graph_" + graph_names[i] + '.pkl'
-    print('Saving ' + outfile + '...' )
-    with open(outfile, 'wb') as handle:
-        pickle.dump(g, handle)
+import pickle
+for i in range(len(names)):
+    with open(f"data/graph_{names[i]}_{n}_{n_obs}.pkl", 'wb') as handle:
+        pickle.dump(graphs[i], handle)

a1b0a7843f1389e12cd8e7fd4299ba0e19a3b685
kristoforusbryant <kristoforusbryant@gmail.com>
2021-07-27 17:31:43 +0800

    Updated experiment routine

--- experiments/5nodes/make_experiments.py
+++ experiments/5nodes/make_experiments.py
@@ -1,57 +1,53 @@
-from dists.Params import GraphAndBasis, Graph
-from dists.CountPriors import TruncatedNB
-from dists.Priors import BasisCount
-from dists.TreePriors import Uniform
-from utils.generate_basis import circle_graph
+from dists.Params import GraphAndBasis as Param
 from tests.test_utils.generate_data import generate_data_and_save
-
+from utils.generate_basis import circle_graph
 import numpy as np
 import networkx as nx
-import pickle
 
 # Number of nodes and observations
-n, n_obs = 5, 250
+n, n_obs = 5, 0
+
+# Graphs to test on
+graphs = []
 
 # Empty Graph
-empty = GraphAndBasis(n)
+graphs.append(Param(n))
 
 # Circle Graph
-circle = GraphAndBasis(n, circle_graph(n))
+g = Param(n, circle_graph(n))
+graphs.append(g.copy())
 
-# Random Graphs from the Cycle space
-print("Generating Graphs from the Cycle space...")
-ct_prior = TruncatedNB(10, .75)
-prior = BasisCount(n, GraphAndBasis, ct_prior, Uniform(n))
+# Random Graph Cycle Space
 np.random.seed(123)
-random0 = prior.Sample()
-random1 = prior.Sample()
+from dists.TreePriors import Hubs as TreePrior
+from dists.Priors import BasisInclusion as Prior
+tree_prior = TreePrior(n)
+prior = Prior(n, Param, alpha=.5, tree_prior=tree_prior)
 
-# Random Graphs from the Cycle space complement
-print("Generating Graphs from the Cycle space complement...")
-random2 = GraphAndBasis(n, nx.to_dict_of_lists(nx.erdos_renyi_graph(n, .2, seed=123)))
-random3 = GraphAndBasis(n, nx.to_dict_of_lists(nx.barabasi_albert_graph(n, 2, seed=123)))
+g = Param(n)
+n_edges = n * (n - 1) // 2
+while g.EdgeCount() <= int(.1 * n_edges) or g.EdgeCount() > int(.4 * n_edges):
+    g = prior.Sample()
+graphs.append(g.copy())
+g = Param(n)
+while g.EdgeCount() <= int(.6 * n_edges) or g.EdgeCount() > int(.9 * n_edges):
+    g = prior.Sample()
+graphs.append(g.copy())
 
-# Complete
-g = Graph(n)
-g.SetComplete()
-complete = GraphAndBasis(n, g._dol)
+# Random Graph
+### change this to only graph in the cycle space
+g = Param(n, nx.to_dict_of_lists(nx.erdos_renyi_graph(n, .2, seed=123)))
+graphs.append(g.copy())
+g = Param(n, nx.to_dict_of_lists(nx.barabasi_albert_graph(n, 2, seed=123)))
+graphs.append(g.copy())
 
-# List of graphs
-graphs = [empty, circle, random0, random1, random2, random3, complete]
-graph_names = ['empty', 'circle', 'random0', 'random1', 'random2', 'random3', 'complete']
+names = ["empty", "circle", "random0", "random1", "random2", "random3"]
+assert(len(names) == len(graphs))
 
-# Generate data
-for i in range(len(graphs)):
-    g = graphs[i]
-    outfile = "data/" + graph_names[i] + '.dat'
-    print('Generating ' + outfile + '...' )
-    generate_data_and_save(n, n_obs, g, outfile, threshold=None, seed=120)
+for i in range(len(names)):
+    generate_data_and_save(n, n_obs, graphs[i], f"data/{names[i]}_{n}_{n_obs}.dat", threshold=None, seed=123)
 
-# Saving graphs
-for i in range(len(graphs)):
-    g = graphs[i]
-    g.SetName(graph_names[i])
-    outfile = "data/graph_" + graph_names[i] + '.pkl'
-    print('Saving ' + outfile + '...' )
-    with open(outfile, 'wb') as handle:
-        pickle.dump(g, handle)
+import pickle
+for i in range(len(names)):
+    with open(f"data/graph_{names[i]}_{n}_{n_obs}.pkl", 'wb') as handle:
+        pickle.dump(graphs[i], handle)
--- experiments/5nodes/runner.py
+++ experiments/5nodes/runner.py
@@ -1,72 +1,34 @@
+from collections import OrderedDict
+import numpy as np
+from utils.config_utils import run_config, get_config_l, config_to_path
 from multiprocessing import Pool
-from utils.cbmcmc import cbmcmc
+import pickle
 
-edgeIO = [ ('data/empty.dat', 'data/graph_empty.pkl', 'results/edge_empty_samples.pkl'),
-           ('data/circle.dat', 'data/graph_circle.pkl', 'results/edge_circle_samples.pkl'),
-           ('data/random0.dat', 'data/graph_random0.pkl', 'results/edge_random0_samples.pkl'),
-           ('data/random1.dat', 'data/graph_random1.pkl', 'results/edge_random1_samples.pkl'),
-           ('data/random2.dat', 'data/graph_random2.pkl', 'results/edge_random2_samples.pkl'),
-           ('data/random3.dat', 'data/graph_random3.pkl', 'results/edge_random3_samples.pkl'),
-           ('data/complete.dat', 'data/graph_complete.pkl', 'results/edge_complete_samples.pkl'), ]
+config = OrderedDict({  'n': [5],
+                        'n_obs': [0],
+                        'init': ['empty'], #'complete',
+                        'true_graph': ["empty"], #, "circle", "random0", "random1", "random2", "random3"],
+                        'prior': ['basis-count'],
+                        'basis': ['hub', 'edge'],
+                        'proposal': ['naive', 'BD'],
+                        'cob_freq': [100],
+                        'iter': [int(1e4)],
+                        'seed': 123 })
 
-allIO = [ ('data/empty.dat', 'data/graph_empty.pkl', 'results/all_empty_samples.pkl'),
-           ('data/circle.dat', 'data/graph_circle.pkl', 'results/all_circle_samples.pkl'),
-           ('data/random0.dat', 'data/graph_random0.pkl', 'results/all_random0_samples.pkl'),
-           ('data/random1.dat', 'data/graph_random1.pkl', 'results/all_random1_samples.pkl'),
-           ('data/random2.dat', 'data/graph_random2.pkl', 'results/all_random2_samples.pkl'),
-           ('data/random3.dat', 'data/graph_random3.pkl', 'results/all_random3_samples.pkl'),
-           ('data/complete.dat', 'data/graph_complete.pkl', 'results/all_complete_samples.pkl'),]
 
-hubIO = [ ('data/empty.dat', 'data/graph_empty.pkl', 'results/hub_empty_samples.pkl'),
-           ('data/circle.dat', 'data/graph_circle.pkl', 'results/hub_circle_samples.pkl'),
-           ('data/random0.dat', 'data/graph_random0.pkl', 'results/hub_random0_samples.pkl'),
-           ('data/random1.dat', 'data/graph_random1.pkl', 'results/hub_random1_samples.pkl'),
-           ('data/random2.dat', 'data/graph_random2.pkl', 'results/hub_random2_samples.pkl'),
-           ('data/random3.dat', 'data/graph_random3.pkl', 'results/hub_random3_samples.pkl'),
-           ('data/complete.dat', 'data/graph_complete.pkl', 'results/hub_complete_samples.pkl'),]
+def run(conf):
+    n, n_obs = conf['n'], conf['n_obs']
+    name = conf['true_graph']
+    data = np.loadtxt(f"data/{name}_{n}_{n_obs}.dat", delimiter=',')
+    sampler = run_config(data, conf)
 
-pathIO = [ ('data/empty.dat', 'data/graph_empty.pkl', 'results/path_empty_samples.pkl'),
-           ('data/circle.dat', 'data/graph_circle.pkl', 'results/path_circle_samples.pkl'),
-           ('data/random0.dat', 'data/graph_random0.pkl', 'results/path_random0_samples.pkl'),
-           ('data/random1.dat', 'data/graph_random1.pkl', 'results/path_random1_samples.pkl'),
-           ('data/random2.dat', 'data/graph_random2.pkl', 'results/path_random2_samples.pkl'),
-           ('data/random3.dat', 'data/graph_random3.pkl', 'results/path_random3_samples.pkl'),
-           ('data/complete.dat', 'data/graph_complete.pkl', 'results/path_complete_samples.pkl'),]
+    with open(f"data/graph_{conf['true_graph']}_{conf['n']}_{conf['n_obs']}.pkl", 'rb') as handle:
+        g = pickle.load (handle)
 
-it = 10000
-r, p = (1, .5)
-cob_freq = 100
-seed = 123
+    for burnin in [0, int(.1 * sampler.iter), int(.25 * sampler.iter)]:
+        print(f"saving to {config_to_path(conf)[:-4]}_burnin-{burnin}.short")
+        with open(config_to_path(conf)[:-4] + f"_burnin-{burnin}.short", 'wb') as handle:
+            pickle.dump(sampler.get_summary(g, burnin, thin=100), handle)
 
-# Edge
-def edge_runner(t):
-    d, i, o = t
-    cbmcmc(d, it, 'edge', r=r, p=p, outfile=o, init=i)
-    return 0
 pool = Pool()
-pool.map(edge_runner, edgeIO)
-
-# All
-# def all_runner(t):
-#     d, i, o = t
-#     cbmcmc(d, it, 'cycle', 'all', r, p, cob_freq, outfile=o, init=i)
-#     return 0
-# pool = Pool()
-# pool.map(all_runner, allIO)
-
-# # Hub
-# def hub_runner(t):
-#     d, i, o = t
-#     cbmcmc(d, it, 'cycle', 'hub', r, p, cob_freq, outfile=o, init=i)
-#     return 0
-# pool = Pool()
-# pool.map(hub_runner, hubIO)
-
-# # Path
-# def path_runner(t):
-#     d, i, o = t
-#     cbmcmc(d, it, 'cycle', 'path', r, p, cob_freq, outfile=o, init=i)
-#     return 0
-# pool = Pool()
-# pool.map(path_runner, pathIO)
-
+pool.map(run, get_config_l(config))
